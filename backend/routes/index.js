3dd00352acd938e596a8b4888ac49462c26d286e
Jacob <premojacob@gmail.com>
2020-12-28 11:50:38 -0500

    Set up skeleton

---
+++ backend/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    router.get('/', (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+    });
+
+    // Serve the static assets in the frontend's build folder
+    router.use(express.static(path.resolve("../frontend/build")));
+
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    router.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('XSRF-TOKEN', req.csrfToken());
+      return res.sendFile(
+        path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+      );
+    });
+  }
+
+
+
+
+
+module.exports = router;
