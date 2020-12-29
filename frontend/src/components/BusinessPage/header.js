b0086feed25e74154521ca7d0a373d3b17407e34
Jacob <premojacob@gmail.com>
2020-12-29 15:37:23 -0500

    Added map to business page

--- frontend/src/components/BusinessPage/header.js
+++ frontend/src/components/BusinessPage/header.js
@@ -1,7 +1,8 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 
 function Header() {
-
+    const tt = window.tt
     let { businessInfo, allImages } = useSelector(state => {
         return ({
             businessInfo: state.business.businessInfo,
@@ -9,6 +10,56 @@ function Header() {
         })
     })
 
+    //history is the history object that allows linking
+    const makeMap = (center, useLinks = true, history = null, ...points) => {
+        let map = tt.map({
+            key: 'g0ZS3ih3olA15iG2cSglfY1YrEJO8DKR',
+            container: 'map',
+            style: 'tomtom://vector/1/basic-main',
+            zoom: center.zoom,
+            center: center.position
+        });
+
+        map.addControl(new tt.FullscreenControl());
+        map.addControl(new tt.NavigationControl());
+
+        var popupOffsets = {
+            top: [0, 0],
+            bottom: [0, -70],
+            'bottom-right': [0, -70],
+            'bottom-left': [0, -70],
+            left: [25, -35],
+            right: [-25, -35]
+        }
+
+        points.forEach((point, ind) => {
+            const marker = new tt.Marker().setLngLat(center.position).addTo(map);
+            const link = 'fill-this-in-later';
+            let popup
+            if (useLinks) {
+                popup = new tt.Popup({ offset: popupOffsets }).setHTML(`<a id="point-${ind}" href="${link}"><b>${point.name}</b></a><br/>${point.address}`);
+            } else {
+                popup = new tt.Popup({ offset: popupOffsets }).setHTML(`<b>${point.name}</b><br/>${point.address}`);
+            }
+            const el = document.getElementById(`point-${ind}`)
+            popup._onMouseUp(() => console.log('test'))
+            marker.setPopup(popup)
+            // marker.togglePopup()
+        })
+
+        return map
+    }
+
+    const setMap = () => {
+        const point = { position: { lng: businessInfo.coordinates.lng, lat: businessInfo.coordinates.lat }, zoom: businessInfo.coordinates.zoom, name: businessInfo.name, address: businessInfo.address }
+        makeMap(point, false, null, point)
+    }
+
+    useEffect(() => {
+        if (businessInfo) {
+            setMap()
+        }
+    })
     if (businessInfo) {
         let arr1 = []
         let arr2 = []
@@ -27,6 +78,7 @@ function Header() {
                 <h1>{businessInfo.name}</h1>
                 <h3>{businessInfo.address}</h3>
                 <p>{businessInfo.description}</p>
+                <div id='map' className='map'>map</div>
             </>
         )
     } else {
