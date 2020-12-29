cf26a8ca14a0daa47876a48519166e12f441cf0b
Jacob <premojacob@gmail.com>
2020-12-29 11:58:41 -0500

    Finished backend query for business info

--- backend/routes/api/business.js
+++ backend/routes/api/business.js
@@ -7,9 +7,26 @@ const { Op } = require('sequelize')
 router.get('/:id', asyncHandler(async (req, res) => {
     const id = Number(req.params.id)
     const business = await Location.findByPk(id, {
-        include: [Image, { model: Post, include: { model: User, include: { model: Image, where: {locationId: id} } } }]
+        include: [Image, { model: Post, include: { model: User, include: { model: Image, where: { locationId: id } } } }]
     });
-    res.json({ id, business })
+    const businessInfo = {
+        businessInfo: { ...business.toJSON() },
+        allImages: business.Images,
+        posts: business.Posts
+    }
+    businessInfo.posts = businessInfo.posts.map(post => {
+        post = post.toJSON()
+        post.images = post.User.Images;
+        delete post.User.Images;
+        post.user = post.User
+        delete post.User
+        return post
+    })
+
+    delete businessInfo.businessInfo.Images
+    delete businessInfo.businessInfo.Posts
+
+    res.json(businessInfo)
 }))
 
 module.exports = router
