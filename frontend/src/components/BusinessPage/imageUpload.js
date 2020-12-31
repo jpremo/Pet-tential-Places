4397a4e87abf011ce49a9378ef0fdb5c8757f17f
Jacob <premojacob@gmail.com>
2020-12-31 11:58:33 -0500

    styled image upload box

--- frontend/src/components/BusinessPage/imageUpload.js
+++ frontend/src/components/BusinessPage/imageUpload.js
@@ -1,5 +1,5 @@
-import {useState} from 'react'
-const ImageUpload = ({uploadedImages, maxSize, setUploadedImages}) => {
+import { useState } from 'react'
+const ImageUpload = ({ uploadedImages, maxSize, setUploadedImages }) => {
 
     const [uploadUrl, setUploadUrl] = useState('')
 
@@ -13,10 +13,20 @@ const ImageUpload = ({uploadedImages, maxSize, setUploadedImages}) => {
     }
 
     const submitUrl = (e) => {
-        const uploadCopy = [...uploadedImages]
-        uploadCopy.push(uploadUrl)
-        setUploadedImages(uploadCopy)
+        if (uploadedImages.length < maxSize) {
+            const uploadCopy = [...uploadedImages]
+            uploadCopy.push(uploadUrl)
+            setUploadedImages(uploadCopy)
+            setUploadUrl('')
+        }
         e.preventDefault()
+    }
+
+    const removePhoto = (e) => {
+        const id = e.target.id.split('-')[1]
+        const uploadCopy = [...uploadedImages]
+        uploadCopy.splice(Number(id), 1)
+        setUploadedImages(uploadCopy)
     }
 
     if (uploadedImages.length === 0) return (
@@ -27,7 +37,7 @@ const ImageUpload = ({uploadedImages, maxSize, setUploadedImages}) => {
             </div>
             <div className='hidden' id='urlInput'>
                 <span>Url:</span>
-                <input value={uploadUrl} onChange={(e) => setUploadUrl(e.target.value)}/>
+                <input value={uploadUrl} onChange={(e) => setUploadUrl(e.target.value)} />
                 <button onClick={submitUrl}>Submit</button>
             </div>
         </>
@@ -40,14 +50,17 @@ const ImageUpload = ({uploadedImages, maxSize, setUploadedImages}) => {
             </div>
             <div className='hidden' id='urlInput'>
                 <span>Url:</span>
-                <input value={uploadUrl} onChange={(e) => setUploadUrl(e.target.value)}/>
+                <input value={uploadUrl} onChange={(e) => setUploadUrl(e.target.value)} />
                 <button onClick={submitUrl}>Submit</button>
             </div>
             <div className='review-image-container'>
                 {uploadedImages.map((el, ind) => {
                     // if (ind >= 1) return (<></>)
                     return (
-                        <img className='review-image' src={el} alt='Uploaded Image' key={ind} id={`newReviewPhoto-${ind}`} />
+                        <div className='review-image-position'>
+                            <img className='review-image' src={el} alt='Uploaded Image' key={ind} id={`newReviewPhoto-${ind}`} />
+                            <i className="fas fa-times-circle fa-1x x-button-3" id={`newReviewPhotoX-${ind}`} style={{ color: 'white', background: 'black', borderRadius: '200px' }} onClick={removePhoto}></i>
+                        </div>
                     )
                 })}
             </div>
