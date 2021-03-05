c4caa65905a85e8a9ef5d6cccc1677b126b1619b
Jacob <premojacob@gmail.com>
2021-03-05 15:07:44 -0500

    Setup footer styling

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -4,11 +4,18 @@ import './Footer.css';
 
 function Footer() {
     return (
-            <div id='footer-container'>
-                <div id='footer'>
-                    Footer
-                 </div>
+        <div id='footer-container'>
+            <div id='footer'>
+                <div>
+                    &copy; Jacob Premo {new Date().getFullYear()}
+                </div>
+                <a href='github.com' className='footer-link'>
+                    <div className='footer-link-image-wrapper'>
+                        <i className="fab fa-github fa-2x"></i>
+                    </div>
+                </a>
             </div>
+        </div>
     );
 }
 
