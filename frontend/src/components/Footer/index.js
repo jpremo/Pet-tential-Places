2f9c3abd0ed7e74f1d2cba1ede00050c202f07fa
Jacob <premojacob@gmail.com>
2021-03-05 15:34:53 -0500

    Finalized footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,14 +6,31 @@ function Footer() {
     return (
         <div id='footer-container'>
             <div id='footer'>
-                <div>
+                <div className='footer-button-wrapper'>
+                    <a href='https://github.com/jpremo' className='footer-link'>
+                        <div className='footer-link-image-wrapper'>
+                            <i className="fab fa-github fa-2x"></i>
+                        </div>
+                    </a>
+                    <a href='https://www.linkedin.com/in/jacob-premo/' className='footer-link'>
+                        <div className='footer-link-image-wrapper'>
+                            <i className="fab fa-linkedin fa-2x"></i>
+                        </div>
+                    </a>
+                    <a href='https://angel.co/u/jacob-premo' className='footer-link'>
+                        <div className='footer-link-image-wrapper'>
+                            <i className="fab fa-angellist fa-2x"></i>
+                        </div>
+                    </a>
+                    <a href='https://www.jacobpremo.com/' className='footer-link'>
+                        <div className='footer-link-image-wrapper'>
+                            <i className="fas fa-briefcase fa-2x"></i>
+                        </div>
+                    </a>
+                </div>
+                <div className='footer-copyright'>
                     &copy; Jacob Premo {new Date().getFullYear()}
                 </div>
-                <a href='github.com' className='footer-link'>
-                    <div className='footer-link-image-wrapper'>
-                        <i className="fab fa-github fa-2x"></i>
-                    </div>
-                </a>
             </div>
         </div>
     );
